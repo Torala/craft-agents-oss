f45751b1058e2a4c11905c65e1912b85cec5ea33
Schipy <schipy@gmail.com>
2025-12-10 15:01:41 +0100

    handle if CRAFT_AGENT_CLI_VERSION is not declared

--- src/agent/options.ts
+++ src/agent/options.ts
@@ -5,7 +5,7 @@ import { homedir } from "os";
 declare const CRAFT_AGENT_CLI_VERSION: string | undefined;
 
 export function getDefaultOptions(): Partial<Options> {
-    if (CRAFT_AGENT_CLI_VERSION != null) {
+    if (typeof CRAFT_AGENT_CLI_VERSION !== 'undefined' && CRAFT_AGENT_CLI_VERSION != null) {
         return {
             pathToClaudeCodeExecutable: join(homedir(), '.local', 'share', 'craft', 'versions', CRAFT_AGENT_CLI_VERSION, 'claude-agent-sdk', 'cli.js')
         }
